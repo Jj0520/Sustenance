2906d87579dac4a2ddf1ed4bce17a6238b279c42
Lim Jia Jun <68286046+Jj0520@users.noreply.github.com>
2025-06-22 18:08:20 +0800

    Delete client directory

--- client/src/constants.js
+++
@@ -1,3 +0,0 @@
-export const NETWORK = "testnet";
-// This should be your wallet address where you deployed the contract
-export const MODULE_ADDRESS = "0xc13e62a4b2451363bd7f23ff81a19f26bd8a4b5a99794265e17592d7f3282876"; 
